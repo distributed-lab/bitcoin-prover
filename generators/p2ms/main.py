89bb74ce46eb935266cb03376ffdd45287339bc9
DmitriiKJ <kurbatovdima2006@gmail.com>
2025-07-10 14:06:16 +0200

    p2ms generator

---
+++ generators/p2ms/main.py
@@ -0,0 +1,76 @@
+import json
+from typing import Dict
+from generators.utils.tx import Transaction
+
+def get_config(path: str = "./generators/p2ms/config.json") -> Dict:
+    with open(path, "r") as f:
+        return json.load(f)
+
+def main():
+    config = get_config()
+
+    curTx = Transaction(config["cur_tx"])
+    curTx.cut_script_sigs()
+
+    vout = curTx.inputs[config["input_to_sign"]].vout
+
+    prevTx = Transaction(config["prev_tx"])
+    prevTx.cut_script_sigs()
+
+    script_pub_key = prevTx.outputs[vout].script_pub_key
+
+    with open(config["file_path"] + "/src/globals.nr.template", "r") as file:
+        templateOpcodes = file.read()
+
+    CUR_TX_INP_COUNT_LEN = curTx._get_compact_size_size(curTx.input_count)
+    CUR_TX_INP_SIZE = sum(curTx._get_input_size(inp) for inp in curTx.inputs) + CUR_TX_INP_COUNT_LEN
+    CUR_TX_OUT_COUNT_LEN = curTx._get_compact_size_size(curTx.output_count)
+    CUR_TX_OUT_SIZE = sum(curTx._get_output_size(out) for out in curTx.outputs) + CUR_TX_OUT_COUNT_LEN
+
+    PREV_TX_INP_COUNT_LEN = prevTx._get_compact_size_size(prevTx.input_count)
+    PREV_TX_INP_SIZE = sum(prevTx._get_input_size(inp) for inp in prevTx.inputs) + PREV_TX_INP_COUNT_LEN
+    PREV_TX_OUT_COUNT_LEN = prevTx._get_compact_size_size(prevTx.output_count)
+    PREV_TX_OUT_SIZE = sum(prevTx._get_output_size(out) for out in prevTx.outputs) + PREV_TX_OUT_COUNT_LEN
+
+    opcodesFile = templateOpcodes.format(
+        curTx=curTx, 
+        prevTx=prevTx,
+        CUR_TX_INP_COUNT_LEN=CUR_TX_INP_COUNT_LEN,
+        CUR_TX_INP_SIZE=CUR_TX_INP_SIZE,
+        CUR_TX_OUT_COUNT_LEN=CUR_TX_OUT_COUNT_LEN,
+        CUR_TX_OUT_SIZE=CUR_TX_OUT_SIZE,
+        PREV_TX_INP_COUNT_LEN=PREV_TX_INP_COUNT_LEN,
+        PREV_TX_INP_SIZE=PREV_TX_INP_SIZE,
+        PREV_TX_OUT_COUNT_LEN=PREV_TX_OUT_COUNT_LEN,
+        PREV_TX_OUT_SIZE=PREV_TX_OUT_SIZE,
+        opcodesAmount=20,
+        curTxLen=curTx._get_transaction_size() * 2, 
+        prevTxLen=prevTx._get_transaction_size() * 2, 
+        signLen=len(config['script_sig']),
+        scriptPubKeyLen=len(script_pub_key),
+        # todo: size can be more than 1 byte
+        n1=script_pub_key[len(script_pub_key) - 2] - 80,
+        m1=script_pub_key[0] - 80
+    )
+
+    with open(config["file_path"] + "/src/globals.nr", "w") as file:
+        file.write(opcodesFile)
+
+    with open(config["file_path"] + "/Prover.toml.template", "r") as file:
+        templateProver = file.read()
+
+    curTxData = curTx.to_hex()
+    prevTxData = prevTx.to_hex()
+        
+    proverFile = templateProver.format(
+        curTxData=curTxData,
+        prevTxData=prevTxData,
+        script_sig=config["script_sig"],
+        input_to_sign=config["input_to_sign"]
+    )
+
+    with open(config["file_path"] + "/Prover.toml", "w") as file:
+        file.write(proverFile)
+
+if __name__ == "__main__":
+    main()
