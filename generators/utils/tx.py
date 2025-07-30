d8eda0ca8b474d8dafa1822abefd915abcdb7c65
Kurbatov Dmytrii <kurbatovdima2006@gmail.com>
2025-07-30 13:15:42 +0200

    feat: p2tr key path spend (#35)
    * add p2tr key path spend
    
    * .

--- generators/utils/tx.py
+++ generators/utils/tx.py
@@ -1,5 +1,10 @@
 from typing import Dict, Union, Tuple
 from collections import namedtuple
+from bitcoin.core import lx, b2x, CTransaction
+from bitcoin.core.serialize import BytesIO
+import requests
+import hashlib
+import sys
 
 Input = namedtuple('Input', ['txid', 'vout', 'script_sig_size', 'script_sig', 'sequence'])
 Output = namedtuple('Output', ['value', 'script_pub_key_size', 'script_pub_key'])
@@ -303,3 +308,44 @@ class Transaction:
             max_witness_stack_size,
             witness_size,
         )
+    
+    def get_outputs_from_inputs(self) -> tuple[list[int], list[Tuple[int, int, int]]]:
+        outs = []
+        pos = []
+        cur_pos = 0
+        txid = self.calculate_txid_from_hex(self.to_hex())
+        response = requests.get(f"https://blockstream.info/api/tx/{txid}")
+        if response.ok:
+            tx_data = response.json()
+            for inp in tx_data["vin"]:
+                amount_bytes = inp["prevout"]["value"].to_bytes(8, byteorder='little')
+                script_pub_key = bytearray.fromhex(inp["prevout"]["scriptpubkey"])
+                script_pub_key_size = bytearray.fromhex(self._get_hex_from_compact_size(len(script_pub_key)))
+
+                pos += [(cur_pos, cur_pos + 8, cur_pos + 8 + len(script_pub_key_size))]
+                cur_pos += 8 + len(script_pub_key_size) + len(script_pub_key)
+
+                outs += amount_bytes + script_pub_key_size + script_pub_key
+        else:
+            print("Error:", response.status_code)
+            sys.exit()
+
+        pos += [(cur_pos, 0, 0)]
+        return (outs, pos)
+
+    def calculate_txid_from_hex(self, raw_hex: str) -> str:
+        tx = CTransaction.stream_deserialize(BytesIO(bytes.fromhex(raw_hex)))
+        return b2x(tx.GetTxid()[::-1])
+
+        
+    def get_outputs_positions_as_toml(self, pos: list[Tuple[int, int, int]]) -> str:
+        res = ""
+        for i in range(len(pos) - 1):
+            res += f"""
+[[utxos_pos]]
+amount = {{offset = {pos[i][0]}, size = {pos[i][1] - pos[i][0]}}}
+script_pub_key_size = {{offset = {pos[i][1]}, size = {pos[i][2] - pos[i][1]}}}
+script_pub_key = {{offset = {pos[i][2]}, size = {pos[i + 1][0] - pos[i][2]}}}
+
+"""
+        return res
--- scripts/p2tr.sh
+++ scripts/p2tr.sh
@@ -3,19 +3,31 @@
 # Exit on error
 set -e
 
+# Create virtual environment and install required packages
+python3 -m venv ./generators/utils/venv
+source ./generators/utils/venv/bin/activate
+pip3 install python-bitcoinlib
+pip3 install requests 
+
 # Generate Prover.toml
-python3 generators/p2tr/main.py
+python3 -m generators.p2tr.main
+
+# Deactivate venv
+deactivate
+
+# Format noir code
+nargo fmt
 
 # Execute the binary
-nargo execute --package p2tr_bin
+nargo execute --package p2tr
 
 # Prove the proof
 mkdir -p target
-mkdir -p target/p2tr_bin
-bb prove -b ./target/p2tr_bin.json -w ./target/p2tr_bin.gz -o ./target/p2tr_bin
+mkdir -p target/p2tr
+bb prove -b ./target/p2tr.json -w ./target/p2tr.gz -o ./target/p2tr
 
 # Write the VK
-bb write_vk -b ./target/p2tr_bin.json -o ./target/p2tr_bin
+bb write_vk -b ./target/p2tr.json -o ./target/p2tr
 
 # Verify the proof
-bb verify -k ./target/p2tr_bin/vk -p ./target/p2tr_bin/proof -i ./target/p2tr_bin/public_inputs
+bb verify -k ./target/p2tr/vk -p ./target/p2tr/proof -i ./target/p2tr/public_inputs
