769dc29dad62ef68a3c75cd17d452525e1879f88
Kurbatov Dmytrii <kurbatovdima2006@gmail.com>
2025-08-12 11:02:36 +0200

    feat: python recursive (#39)
    * add python recursive prooving
    
    * add logs
    
    * fixes
    
    * added block height, chainwork. proved 6 blocks after new target
    
    * fixed python main
    
    * fix python script
    
    * remove some public inputs
    
    * fix timestamp calc
    
    * now it's median timestamp (not avarage)

--- generators/blocks/main.py
+++ generators/blocks/main.py
@@ -3,6 +3,10 @@ from typing import Dict
 from pullers import BlockHeaderPuller
 from block import Block, create_nargo_toml
 import logging
+import sys
+import subprocess
+import ast
+import logging
 
 def setup_logging():
     logging.basicConfig(level=logging.DEBUG)
@@ -14,28 +18,131 @@ def get_config(path: str = "./generators/blocks/config.json") -> Dict:
 def main():
     setup_logging()
     config = get_config()
+    index = 0
+
+    blocks_amount = config["blocks"]["count"]
+
+    if blocks_amount % 2016 != 6:
+        print("Amount of blocks must be (2016 * x + 6), where x > 0")
+        sys.exit()
 
     puller = BlockHeaderPuller(config["gateway"])
     hex_headers =puller.pull_block_headers(config["blocks"]["start"], config["blocks"]["count"])
     blocks = [Block(header) for header in hex_headers]
 
-    with open(config["nargo"]["file_path"] + "/src/main.nr", "w") as f:
-        f.write(f"""use blocks_lib::{{block::{{BlockHeader, get_block_hash}}, chain::check_chain}};
+    nargo_toml = create_nargo_toml(blocks[index:2022], "blocks")
+    index = 2021
 
-fn main(blocks: [BlockHeader; {len(blocks)}], last_block_hash: pub str<64>) -> pub bool {{
-    assert(
-        get_block_hash(blocks[0])
-            == "000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f",
-    );
-    assert(get_block_hash(blocks[blocks.len() - 1]) == last_block_hash);
-    check_chain(blocks)
-}}
-""")
-
-    nargo_toml = create_nargo_toml(blocks, config["nargo"]["struct_name"])
-    with open(config["nargo"]["file_path"] + "/Prover.toml", "w") as f:
-        f.write(f"last_block_hash = \"{blocks[len(blocks) - 1].get_block_hash()}\"\n\n")
+    with open("./app/blocks-recursive/start/Prover.toml", "w") as f:
+        f.write(f"last_block_hash = \"{blocks[index].get_block_hash()}\"\n\n")
         f.write(nargo_toml)
 
+    logging.debug("nargo execute (start)")
+    subprocess.run(['nargo', 'execute', '--package', 'start'], check=True)
+
+    logging.debug("bb proof")
+    subprocess.run(['bb', 'prove', 
+                    '-s', 'ultra_honk', 
+                    '-b', './target/start.json', 
+                    '-w', './target/start.gz', 
+                    '-o', './target/blocks_bin', 
+                    '--output_format', 'bytes_and_fields', 
+                    '--honk_recursion', '1', 
+                    '--recursive', 
+                    '--init_kzg_accumulator'],
+                    check=True)
+    
+    logging.debug("bb write_vk (start)")
+    subprocess.run(['bb', 'write_vk', 
+                    '-s', 'ultra_honk', 
+                    '-b', './target/start.json', 
+                    '-o', './target/blocks_bin', 
+                    '--output_format', 'bytes_and_fields', 
+                    '--honk_recursion', '1', 
+                    '--init_kzg_accumulator'],
+                    check=True)
+    
+    subprocess.run(['bb', 'verify', 
+                    '-s', 'ultra_honk', 
+                    '-k', './target/blocks_bin/vk', 
+                    '-p', './target/blocks_bin/proof', 
+                    '-i', './target/blocks_bin/public_inputs'],
+                    check=True)
+    
+    with open("./target/blocks_bin/proof_fields.json", "r") as file:
+        proof = file.read()
+
+    with open("./target/blocks_bin/vk_fields.json", "r") as file:
+        vk = file.read()
+
+    with open("./target/blocks_bin/public_inputs_fields.json", "r") as file:
+        pi = file.read()
+
+    logging.debug("nargo compile (recursive)")
+    subprocess.run(['nargo', 'compile', '--package', 'rec'], check=True)
+
+    logging.debug("bb write_vk (recursive)")
+    subprocess.run(['bb', 'write_vk', 
+                    '-s', 'ultra_honk', 
+                    '-b', './target/rec.json', 
+                    '-o', './target/blocks_bin/rec', 
+                    '--output_format', 'bytes_and_fields', 
+                    '--honk_recursion', '1', 
+                    '--init_kzg_accumulator'],
+                    check=True)
+    
+    with open("./target/blocks_bin/rec/vk_fields.json", "r") as file:
+        vk_rec = file.read()
+    
+    while index < (blocks_amount - 1):
+        pi_array = ast.literal_eval(pi)
+
+        logging.debug(f"Prooving blocks from {index - 5} to {index + 2016}")
+        nargo_toml = create_nargo_toml(blocks[(index - 5):(index + 2017)], "blocks")
+        index += 2016
+
+        with open("./app/blocks-recursive/rec/Prover.toml", "w") as f:
+            f.write(f"last_block_hash = \"{blocks[index].get_block_hash()}\"\n\n")
+            f.write(f"timestamps = [{', '.join(str(v) for v in pi_array[64:75])}]\n\n")
+            f.write(f"time_idx = \"{pi_array[-3]}\"\n\n")
+            f.write(f"last_block_height = \"{int(pi_array[-2], 16)}\"\n\n")
+            f.write(f"chainwork = \"{int(pi_array[-1], 16)}\"\n\n")
+            f.write(f"verification_key = {vk}\n\n")
+            f.write(f"proof = {proof}\n\n")
+            f.write(f"public_inputs = {pi}\n\n")
+            f.write(nargo_toml)
+
+        logging.debug("nargo execute (recursive)")
+        subprocess.run(['nargo', 'execute', '--package', 'rec'], check=True)
+
+        logging.debug("bb prove (recursive)")
+        subprocess.run(['bb', 'prove', 
+                        '-s', 'ultra_honk', 
+                        '-b', './target/rec.json', 
+                        '-w', './target/rec.gz', 
+                        '-o', './target/blocks_bin/rec', 
+                        '--output_format', 'bytes_and_fields', 
+                        '--honk_recursion', '1', 
+                        '--recursive', 
+                        '--init_kzg_accumulator'],
+                        check=True)
+        
+        subprocess.run(['bb', 'verify', 
+                        '-s', 'ultra_honk', 
+                        '-k', './target/blocks_bin/rec/vk', 
+                        '-p', './target/blocks_bin/rec/proof', 
+                        '-i', './target/blocks_bin/rec/public_inputs'],
+                        check=True)
+        
+        with open("./target/blocks_bin/rec/proof_fields.json", "r") as file:
+            proof = file.read()
+
+        vk = vk_rec
+
+        with open("./target/blocks_bin/rec/public_inputs_fields.json", "r") as file:
+            pi = file.read()
+
+    print("Recursive proof was created successfully")
+    
 if __name__ == "__main__":
     main()
--- scripts/blocks.sh
+++ scripts/blocks.sh
@@ -3,6 +3,10 @@
 # Exit on error
 set -e  
 
+# Create required folders
+mkdir -p ./target/blocks_bin
+mkdir -p ./target/blocks_bin/rec
+
 # Create virtual environment and install required packages
 python3 -m venv ./generators/blocks/venv
 source ./generators/blocks/venv/bin/activate
@@ -13,17 +17,3 @@ python3 generators/blocks/main.py
 
 # Deactivate venv
 deactivate
-
-# Execute the binary
-nargo execute --package blocks_bin
-
-# Prove the proof
-mkdir -p target
-mkdir -p target/blocks_bin
-bb prove -b ./target/blocks_bin.json -w ./target/blocks_bin.gz -o ./target/blocks_bin
-
-# Write the VK
-bb write_vk -b ./target/blocks_bin.json -o ./target/blocks_bin
-
-# Verify the proof
-bb verify -k ./target/blocks_bin/vk -p ./target/blocks_bin/proof -i ./target/blocks_bin/public_inputs
