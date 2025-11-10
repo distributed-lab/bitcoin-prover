d596a6ff96f703b2a68b4024dc127b18e7e9a420
DmitriiKJ <kurbatovdima2006@gmail.com>
2025-11-10 17:06:13 +0100

    fmt

--- utxo_indexer/proof_of_reserve/src/main.rs
+++ utxo_indexer/proof_of_reserve/src/main.rs
@@ -1,11 +1,11 @@
+use anyhow::{Ok, Result};
 use k256::{ecdsa::SigningKey, elliptic_curve::sec1::ToEncodedPoint};
 use serde::{Deserialize, Serialize};
 use sha2::{Digest, Sha256};
-use utxo_test_data_generator::test_data_gen::{TestUtxo, generate_test_utxos};
 use std::fs::{self, File};
 use std::io::Write;
-use anyhow::{Ok, Result};
 use std::process::Command;
+use utxo_test_data_generator::test_data_gen::{TestUtxo, generate_test_utxos};
 
 const MAX_COINS_DATABASE_AMOUNT: usize = 5;
 const MAX_NODES_AMOUNT: usize = 8;
@@ -38,7 +38,7 @@ struct NodeProof {
 #[derive(Serialize, Deserialize)]
 struct NodeToml {
     verification_key: Vec<String>,
-    node_proofs: Vec<NodeProof> 
+    node_proofs: Vec<NodeProof>,
 }
 
 fn main() {
@@ -109,41 +109,55 @@ fn main() {
         .expect("failed to execute command");
 
     assert!(status.success(), "Command return non-zero status");
-
 }
 
 fn leafs_toml(utxos: Vec<TestUtxo>, message_hash: &[u8; 32], public_key: &[u8]) -> Result<()> {
-    let mut coins_database: Vec<CoinsDatabaseElement> = utxos.iter().map(|e| CoinsDatabaseElement { script_pub_key: hex::decode(&e.script_pub_key).unwrap(), amount: e.amount } ).collect();
+    let mut coins_database: Vec<CoinsDatabaseElement> = utxos
+        .iter()
+        .map(|e| CoinsDatabaseElement {
+            script_pub_key: hex::decode(&e.script_pub_key).unwrap(),
+            amount: e.amount,
+        })
+        .collect();
 
     let append_from = coins_database.len();
-    let append_to = ((coins_database.len() + MAX_COINS_DATABASE_AMOUNT - 1) / MAX_COINS_DATABASE_AMOUNT) * MAX_COINS_DATABASE_AMOUNT;
+    let append_to = ((coins_database.len() + MAX_COINS_DATABASE_AMOUNT - 1)
+        / MAX_COINS_DATABASE_AMOUNT)
+        * MAX_COINS_DATABASE_AMOUNT;
 
     for _ in append_from..append_to {
-        coins_database.push(CoinsDatabaseElement { script_pub_key: Vec::from([0; 25]), amount: 0 });
+        coins_database.push(CoinsDatabaseElement {
+            script_pub_key: Vec::from([0; 25]),
+            amount: 0,
+        });
     }
 
     let mut own_utxos: Vec<Spending> = utxos
-            .iter()
-            .map(|e| {
-                let mut witness = hex::decode(&e.witness).unwrap();
-                if witness.len() < 72 {
-                    witness.resize(72, 0);
-                }
+        .iter()
+        .map(|e| {
+            let mut witness = hex::decode(&e.witness).unwrap();
+            if witness.len() < 72 {
+                witness.resize(72, 0);
+            }
 
-                let mut pub_key = Vec::from(public_key);
-                if pub_key.len() < 65 {
-                    pub_key.resize(65, 0);
-                }
+            let mut pub_key = Vec::from(public_key);
+            if pub_key.len() < 65 {
+                pub_key.resize(65, 0);
+            }
 
-                Spending { witness, pub_key }
-            })
+            Spending { witness, pub_key }
+        })
         .collect();
 
     let append_from = own_utxos.len();
-    let append_to = ((own_utxos.len() + MAX_COINS_DATABASE_AMOUNT - 1) / MAX_COINS_DATABASE_AMOUNT) * MAX_COINS_DATABASE_AMOUNT;
+    let append_to = ((own_utxos.len() + MAX_COINS_DATABASE_AMOUNT - 1) / MAX_COINS_DATABASE_AMOUNT)
+        * MAX_COINS_DATABASE_AMOUNT;
 
     for _ in append_from..append_to {
-        own_utxos.push(Spending { witness: Vec::from([0; 72]), pub_key: Vec::from([0; 65]) });
+        own_utxos.push(Spending {
+            witness: Vec::from([0; 72]),
+            pub_key: Vec::from([0; 65]),
+        });
     }
 
     let toml_struct = LeafsToml {
@@ -169,12 +183,10 @@ fn tree_toml() -> Result<()> {
 
     let node_toml = NodeToml {
         verification_key: vk_fields,
-        node_proofs: vec![
-            NodeProof {
-                proof: proof_fields,
-                public_inputs: pi_fields
-            }
-        ]
+        node_proofs: vec![NodeProof {
+            proof: proof_fields,
+            public_inputs: pi_fields,
+        }],
     };
 
     let mut file = File::create("../circuits/app/proof_of_reserve/utxos_tree/Prover.toml")?;
