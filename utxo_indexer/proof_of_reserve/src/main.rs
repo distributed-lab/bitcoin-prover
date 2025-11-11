2bae6f3acb64e7f07cfe32145a0d410cb4d85b35
DmitriiKJ <kurbatovdima2006@gmail.com>
2025-11-11 12:13:57 +0100

    update noir version (to fix bug with ecdsa verify)

--- utxo_indexer/proof_of_reserve/src/main.rs
+++ utxo_indexer/proof_of_reserve/src/main.rs
@@ -38,13 +38,14 @@ struct NodeProof {
 #[derive(Serialize, Deserialize)]
 struct NodeToml {
     verification_key: Vec<String>,
+    key_hash: String,
     node_proofs: Vec<NodeProof>,
 }
 
 fn main() {
     let message = "Hello, world!";
     let priv_key = [1; 32];
-    let utxos = generate_test_utxos(5, message.as_ref(), &priv_key).unwrap();
+    let utxos = generate_test_utxos(4, message.as_ref(), &priv_key).unwrap();
 
     let sk = SigningKey::from_bytes(&priv_key).unwrap();
     let pk = sk.verifying_key();
@@ -70,7 +71,7 @@ fn main() {
 
     let status = Command::new("bash")
         .arg("-c")
-        .arg("bb write_vk -b ../../../target/coins.json -o ../../../target/vk/leafs --output_format bytes_and_fields --honk_recursion 1 --init_kzg_accumulator")
+        .arg("bb write_vk -b ../../../target/coins.json -o ../../../target/vk/leafs")
         .current_dir("../circuits/app/proof_of_reserve/coins")
         .status()
         .expect("failed to execute command");
@@ -81,7 +82,7 @@ fn main() {
 
     let status = Command::new("bash")
         .arg("-c")
-        .arg("bb prove -b ../../../target/coins.json -w ../../../target/coins.gz -o ../../../target/proof/leafs --output_format bytes_and_fields --honk_recursion 1 --recursive --init_kzg_accumulator")
+        .arg("bb prove -b ../../../target/coins.json -w ../../../target/coins.gz -o ../../../target/proof/leafs -k ../../../target/vk/leafs/vk")
         .current_dir("../circuits/app/proof_of_reserve/coins")
         .status()
         .expect("failed to execute command");
@@ -103,7 +104,16 @@ fn main() {
 
     let status = Command::new("bash")
         .arg("-c")
-        .arg("bb prove -b ../../../target/utxos_tree.json -w ../../../target/utxos_tree.gz -o ../../../target/proof/leafs --output_format bytes_and_fields --honk_recursion 1 --recursive --init_kzg_accumulator")
+        .arg("bb write_vk -b ../../../target/utxos_tree.json -o ../../../target/vk/tree")
+        .current_dir("../circuits/app/proof_of_reserve/utxos_tree")
+        .status()
+        .expect("failed to execute command");
+
+    assert!(status.success(), "Command return non-zero status");
+
+    let status = Command::new("bash")
+        .arg("-c")
+        .arg("bb prove -b ../../../target/utxos_tree.json -w ../../../target/utxos_tree.gz -o ../../../target/proof/leafs -k ../../../target/vk/tree/vk")
         .current_dir("../circuits/app/proof_of_reserve/utxos_tree")
         .status()
         .expect("failed to execute command");
@@ -132,6 +142,11 @@ fn leafs_toml(utxos: Vec<TestUtxo>, message_hash: &[u8; 32], public_key: &[u8]) 
         });
     }
 
+    let mut pub_key = Vec::from(public_key);
+    if pub_key.len() < 65 {
+        pub_key.resize(65, 0);
+    }
+
     let mut own_utxos: Vec<Spending> = utxos
         .iter()
         .map(|e| {
@@ -140,12 +155,7 @@ fn leafs_toml(utxos: Vec<TestUtxo>, message_hash: &[u8; 32], public_key: &[u8]) 
                 witness.resize(72, 0);
             }
 
-            let mut pub_key = Vec::from(public_key);
-            if pub_key.len() < 65 {
-                pub_key.resize(65, 0);
-            }
-
-            Spending { witness, pub_key }
+            Spending { witness, pub_key: pub_key.clone() }
         })
         .collect();
 
@@ -173,19 +183,33 @@ fn leafs_toml(utxos: Vec<TestUtxo>, message_hash: &[u8; 32], public_key: &[u8]) 
 }
 
 fn tree_toml() -> Result<()> {
-    let proof_string = fs::read_to_string("../circuits/target/proof/leafs/proof_fields.json")?;
-    let pi_string = fs::read_to_string("../circuits/target/proof/leafs/public_inputs_fields.json")?;
-    let vk_string = fs::read_to_string("../circuits/target/vk/leafs/vk_fields.json")?;
+    let proof = fs::read("../circuits/target/proof/leafs/proof")?;
+    let pi = fs::read("../circuits/target/proof/leafs/public_inputs")?;
+    let vk = fs::read("../circuits/target/vk/leafs/vk")?;
+    let vk_hash = fs::read("../circuits/target/vk/leafs/vk_hash")?;
 
-    let proof_fields: Vec<String> = serde_json::from_str(&proof_string)?;
-    let pi_fields: Vec<String> = serde_json::from_str(&pi_string)?;
-    let vk_fields: Vec<String> = serde_json::from_str(&vk_string)?;
+    let mut proof_strings: Vec<String> = Vec::new();
+    let mut pi_strings: Vec<String> = Vec::new();
+    let mut vk_strings: Vec<String> = Vec::new();
+
+    for byte in proof.chunks(32) {
+        proof_strings.push("0x".to_owned() + &hex::encode(byte));
+    }
+
+    for byte in pi.chunks(32) {
+        pi_strings.push("0x".to_owned() + &hex::encode(byte));
+    }
+
+    for byte in vk.chunks(32) {
+        vk_strings.push("0x".to_owned() + &hex::encode(byte));
+    }
 
     let node_toml = NodeToml {
-        verification_key: vk_fields,
+        verification_key: vk_strings,
+        key_hash: "0x".to_owned() + &hex::encode(vk_hash),
         node_proofs: vec![NodeProof {
-            proof: proof_fields,
-            public_inputs: pi_fields,
+            proof: proof_strings,
+            public_inputs: pi_strings,
         }],
     };
 
